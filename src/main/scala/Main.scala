058ae7af2f806d179aff1b28e19ec636d0d35cc3
sesteves <sroesteves@gmail.com>
2016-04-05 20:31:44 +0100

    minor

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,12 @@
+import java.util.concurrent.TimeUnit
+
+import org.apache.flink.api.common.functions.JoinFunction
+import org.apache.flink.streaming.api.datastream.{DataStream, MultiWindowsJoinedStreams}
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.windowing.assigners.{SlidingTimeWindows, TumblingTimeWindows}
+import org.apache.flink.streaming.api.windowing.time.Time
+
+import scala.collection.JavaConverters._
 
 /**
  * Created by sesteves on 17-03-2016.
@@ -14,10 +22,20 @@ object Main {
 
 
 
+      val joinedStream = new MultiWindowsJoinedStreams(rawStream1, rawStream2)
+
+
+
+      val stream = joinedStream
+        .where(new MyFirstKeySelector())
+        .window(SlidingTimeWindows.of(Time.of(9, TimeUnit.SECONDS), Time.of(3, TimeUnit.SECONDS)))
+        .equalTo(new MySecondKeySelector())
+        .window(TumblingTimeWindows.of(Time.of(3, TimeUnit.SECONDS)))
+        .apply(new JoinFunction());
+
 
       env.execute("flink-test")
     }
-
 
 
 
