6174db5b52f2d91bac2f15b5bc7e5b2fe5b5a714
sesteves <sroesteves@gmail.com>
2016-04-12 20:17:30 +0100

    Working example

--- src/main/scala/EventTimeData.scala
+++ src/main/scala/EventTimeData.scala
@@ -4,48 +4,48 @@
 
 object EventTimeData {
 
-  public static List<Tuple3<Long, String, Integer>> personWithAge = Arrays.asList(
-    new Tuple3<>(1448892364000L, "Miko", 15), new Tuple3<>(1448892367000L, "Jace", 25),
-    new Tuple3<>(1448892368000L, "Rose", 25), new Tuple3<>(1448892372000L, "Hari", 43),
-    new Tuple3<>(1448892374000L, "Jack", 35), new Tuple3<>(1448892378000L, "Jason", 37),
-    new Tuple3<>(1448892382000L, "Mick", 21), new Tuple3<>(1448892389000L, "Kk", 25),
-    new Tuple3<>(1448892390000L, "Duke", 25), new Tuple3<>(1448892399000L, "Duma", 28),
-    new Tuple3<>(1448892404000L, "Anna", 22), new Tuple3<>(1448892406000L, "Erik", 30),
-    new Tuple3<>(1448892412000L, "Jarn", 35), new Tuple3<>(1448892414000L, "Nick", 37),
-    new Tuple3<>(1448892414040L, "Kuke", 23), new Tuple3<>(1448892415100L, "Json", 39),
-    new Tuple3<>(1448892417400L, "Deak", 28), new Tuple3<>(1448892423010L, "Jim", 29),
-    new Tuple3<>(1448892425110L, "Obama", 55), new Tuple3<>(1448892425810L, "Busi", 65),
-    new Tuple3<>(1448892427010L, "Jook", 34), new Tuple3<>(1448892429110L, "Nkin", 25),
-    new Tuple3<>(1448892430110L, "Mari", 25), new Tuple3<>(1448892432110L, "Mike", 26),
-    new Tuple3<>(1448892436110L, "Uoks", 28), new Tuple3<>(1448892446110L, "Duli", 95),
-    new Tuple3<>(1448892504000L, "Mkke", 25), new Tuple3<>(1448892506500L, "Kom", 45),
-    new Tuple3<>(1448892508200L, "Niok", 32), new Tuple3<>(1448892509000L, "Jeep", 34),
-    new Tuple3<>(1448892512300L, "Ikls", 25), new Tuple3<>(1448892515300L, "Lksa", 12),
-    new Tuple3<>(1448892517300L, "LJks", 22), new Tuple3<>(1448892537300L, "Luks", 15)
-  );
+  val personWithAge = Seq(
+    Tuple3(1448892364000L, "Miko", 15), Tuple3(1448892367000L, "Jace", 25),
+    Tuple3(1448892368000L, "Rose", 25), Tuple3(1448892372000L, "Hari", 43),
+    Tuple3(1448892374000L, "Jack", 35), Tuple3(1448892378000L, "Jason", 37),
+    Tuple3(1448892382000L, "Mick", 21), Tuple3(1448892389000L, "Kk", 25),
+    Tuple3(1448892390000L, "Duke", 25), Tuple3(1448892399000L, "Duma", 28),
+    Tuple3(1448892404000L, "Anna", 22), Tuple3(1448892406000L, "Erik", 30),
+    Tuple3(1448892412000L, "Jarn", 35), Tuple3(1448892414000L, "Nick", 37),
+    Tuple3(1448892414040L, "Kuke", 23), Tuple3(1448892415100L, "Json", 39),
+    Tuple3(1448892417400L, "Deak", 28), Tuple3(1448892423010L, "Jim", 29),
+    Tuple3(1448892425110L, "Obama", 55), Tuple3(1448892425810L, "Busi", 65),
+    Tuple3(1448892427010L, "Jook", 34), Tuple3(1448892429110L, "Nkin", 25),
+    Tuple3(1448892430110L, "Mari", 25), Tuple3(1448892432110L, "Mike", 26),
+    Tuple3(1448892436110L, "Uoks", 28), Tuple3(1448892446110L, "Duli", 95),
+    Tuple3(1448892504000L, "Mkke", 25), Tuple3(1448892506500L, "Kom", 45),
+    Tuple3(1448892508200L, "Niok", 32), Tuple3(1448892509000L, "Jeep", 34),
+    Tuple3(1448892512300L, "Ikls", 25), Tuple3(1448892515300L, "Lksa", 12),
+    Tuple3(1448892517300L, "LJks", 22), Tuple3(1448892537300L, "Luks", 15)
+  )
 
   // Rose 10s later, Hari 5s later, Anna 3s earlier, Ikls 30s later
   // Make missing
-  public static List<Tuple3<Long, String, String>> personWithInterest = Arrays.asList(
-    new Tuple3<>(1448892363000L, "Miko", "Apple"), new Tuple3<>(1448892368000L, "Jace", "Game"),
-    new Tuple3<>(1448892375000L, "Rose", "Organge"), new Tuple3<>(1448892375000L, "Hari", "Banana"),
-    new Tuple3<>(1448892375200L, "Jack", "Chatting"), new Tuple3<>(1448892380000L, "Jason", "Walking"),
-    new Tuple3<>(1448892382000L, "Mick", "Egg"), new Tuple3<>(1448892389000L, "Kk", "Cabbage"),
-    new Tuple3<>(1448892390400L, "Duke", "Chilli"), new Tuple3<>(1448892401000L, "Duma", "Reading"),
-    new Tuple3<>(1448892401000L, "Anna", "Cycling"), new Tuple3<>(1448892406000L, "Erik", "Kayka"),
-    new Tuple3<>(1448892412000L, "Jarn", "Singing"), new Tuple3<>(1448892414000L, "Nick", "Reading"),
-    new Tuple3<>(1448892414040L, "Kuke", "iPhone"), new Tuple3<>(1448892415100L, "Json", "Mac"),
+  val personWithInterest = Seq(
+    Tuple3(1448892363000L, "Miko", "Apple"), Tuple3(1448892368000L, "Jace", "Game"),
+    Tuple3(1448892375000L, "Rose", "Organge"), Tuple3(1448892375000L, "Hari", "Banana"),
+    Tuple3(1448892375200L, "Jack", "Chatting"), Tuple3(1448892380000L, "Jason", "Walking"),
+    Tuple3(1448892382000L, "Mick", "Egg"), Tuple3(1448892389000L, "Kk", "Cabbage"),
+    Tuple3(1448892390400L, "Duke", "Chilli"), Tuple3(1448892401000L, "Duma", "Reading"),
+    Tuple3(1448892401000L, "Anna", "Cycling"), Tuple3(1448892406000L, "Erik", "Kayka"),
+    Tuple3(1448892412000L, "Jarn", "Singing"), Tuple3(1448892414000L, "Nick", "Reading"),
+    Tuple3(1448892414040L, "Kuke", "iPhone"), Tuple3(1448892415100L, "Json", "Mac"),
 
-    new Tuple3<>(1448892415240L, "Kuse", "OnePlus"), new Tuple3<>(1448892415400L, "Jsonn", "iMac"),
+    Tuple3(1448892415240L, "Kuse", "OnePlus"), Tuple3(1448892415400L, "Jsonn", "iMac"),
 
-    new Tuple3<>(1448892419400L, "Deak", "Stories"), new Tuple3<>(1448892423910L, "Jim", "Pating"),
-    new Tuple3<>(1448892425110L, "Obama", "Dawing"), new Tuple3<>(1448892425910L, "Busi", "Coding"),
-    new Tuple3<>(1448892427310L, "Jook", "Smiles"), new Tuple3<>(1448892429110L, "Nkin", "Jump"),
-    new Tuple3<>(1448892434110L, "Mari", "Shopping"), new Tuple3<>(1448892439110L, "Mike", "Forest"),
-    new Tuple3<>(1448892446110L, "Uoks", "Lake"), new Tuple3<>(1448892446310L, "Duli", "Boat"),
-    new Tuple3<>(1448892504000L, "Mkke", "Music"), new Tuple3<>(1448892506900L, "Kom", "Food"),
-    new Tuple3<>(1448892509200L, "Niok", "Learing"), new Tuple3<>(1448892511000L, "Jeep", "Games"),
-    new Tuple3<>(1448892532300L, "Ikls", "Warcraft"), new Tuple3<>(1448892534300L, "Lksa", "Dota"),
-    new Tuple3<>(1448892536300L, "LJks", "Age3"), new Tuple3<>(1448892568300L, "Luks", "nothing")
+    Tuple3(1448892419400L, "Deak", "Stories"), Tuple3(1448892423910L, "Jim", "Pating"),
+    Tuple3(1448892425110L, "Obama", "Dawing"), Tuple3(1448892425910L, "Busi", "Coding"),
+    Tuple3(1448892427310L, "Jook", "Smiles"), Tuple3(1448892429110L, "Nkin", "Jump"),
+    Tuple3(1448892434110L, "Mari", "Shopping"), Tuple3(1448892439110L, "Mike", "Forest"),
+    Tuple3(1448892446110L, "Uoks", "Lake"), Tuple3(1448892446310L, "Duli", "Boat"),
+    Tuple3(1448892504000L, "Mkke", "Music"), Tuple3(1448892506900L, "Kom", "Food"),
+    Tuple3(1448892509200L, "Niok", "Learing"), Tuple3(1448892511000L, "Jeep", "Games"),
+    Tuple3(1448892532300L, "Ikls", "Warcraft"), Tuple3(1448892534300L, "Lksa", "Dota"),
+    Tuple3(1448892536300L, "LJks", "Age3"), Tuple3(1448892568300L, "Luks", "nothing")
   )
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,6 +3,7 @@ import java.util.Properties
 
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.api.common.functions.JoinFunction
+import org.apache.flink.api.java.tuple.Tuple3
 import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.datastream.MultiWindowsJoinedStreams
@@ -12,96 +13,118 @@ import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.connectors.kafka.{FlinkKafkaConsumer, FlinkKafkaConsumer082}
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema
 import org.apache.flink.api.java.functions.KeySelector
-
+import org.apache.flink.streaming.api.functions.TimestampExtractor
 
 
 /**
- * Created by sesteves on 17-03-2016.
- */
+  * Created by sesteves on 17-03-2016.
+  */
 object Main {
 
 
-    def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit = {
 
-      // Based on https://github.com/yahoo/streaming-benchmarks/blob/master/flink-benchmarks/src/main/java/flink/benchmark/AdvertisingTopologyNative.java
+    // Based on https://github.com/yahoo/streaming-benchmarks/blob/master/flink-benchmarks/src/main/java/flink/benchmark/AdvertisingTopologyNative.java
 
+    val kafkaPartitions = 1
+    val hosts = 1
+    val cores = 8
+    /*
+          // val params = ParameterTool.fromArgs(args)
+          // val params = ParameterTool.fromSystemProperties()
+          val properties = new Properties()
+          properties.setProperty("bootstrap.servers", "ginja-a1:9092")
+          properties.setProperty("zookeeper.connect", "ginja-a1:2181")
+          properties.setProperty("group.id", "test")
+          properties.put("topic", "ad-events");
+          properties.put("auto.offset.reset", "smallest");
+    //      props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    //      props.put(ProducerConfig.RETRIES_CONFIG, "3");
+    //      props.put(ProducerConfig.ACKS_CONFIG, "all");
+    //      props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "none");
+    //      props.put(ProducerConfig.BATCH_SIZE_CONFIG, 200);
+    //      props.put(ProducerConfig.BLOCK_ON_BUFFER_FULL_CONFIG, true);
+    //      props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+    //      props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+    */
 
-      val kafkaPartitions = 1
-      val hosts = 1
-      val cores = 8
-
-      // val params = ParameterTool.fromArgs(args)
-      // val params = ParameterTool.fromSystemProperties()
-      val properties = new Properties()
-      properties.setProperty("bootstrap.servers", "ginja-a1:9092")
-      properties.setProperty("zookeeper.connect", "ginja-a1:2181")
-      properties.setProperty("group.id", "test")
-      properties.put("topic", "ad-events");
-      properties.put("auto.offset.reset", "smallest");
-//      props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-//      props.put(ProducerConfig.RETRIES_CONFIG, "3");
-//      props.put(ProducerConfig.ACKS_CONFIG, "all");
-//      props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "none");
-//      props.put(ProducerConfig.BATCH_SIZE_CONFIG, 200);
-//      props.put(ProducerConfig.BLOCK_ON_BUFFER_FULL_CONFIG, true);
-//      props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-//      props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-
-
-      val env = StreamExecutionEnvironment.getExecutionEnvironment
-      env.setParallelism(hosts * cores)
-
-
-
-      val messageStream = env
-        .addSource(new FlinkKafkaConsumer082[String]("ad-events", new SimpleStringSchema(), properties))
-        .setParallelism(Math.min(hosts * cores, kafkaPartitions))
-
-      messageStream.rebalance
-
-
-//      val rawStream1 = env.socketTextStream("ginja-a1", 8800, maxRetry = -1)
-//      val rawStream2 = env.socketTextStream("ginja-a1", 8800, maxRetry = -1)
-
-
-      // Based on https://github.com/wangyangjun/flink-stream-join/blob/master/src/test/java/fi/aalto/dmg/KafkaWindowJoinTest.java
-
-      env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
-
-
-      val joinedStream = new MultiWindowsJoinedStreams(messageStream.getJavaStream, messageStream.getJavaStream)
-
-      val keySelector = new KeySelector[String, String] {
-        override def getKey(value: String): String = return value
-      }
-
-      joinedStream.where(keySelector)
-        .window(SlidingTimeWindows.of(Time.of(25, TimeUnit.SECONDS), Time.of(5, TimeUnit.SECONDS)))
-        .equalTo(keySelector)
-        .window(TumblingTimeWindows.of(Time.of(5, TimeUnit.SECONDS)))
-
-
-        .apply(new JoinFunction<Tuple2<String, Long>, Tuple2<String, Long>, Tuple3<String, Long, Long>>() {
-          private static final long serialVersionUID = -3625150954096822268L;
-
-          @Override
-          public Tuple3<String, Long, Long> join(Tuple2<String, Long> first, Tuple2<String, Long> second) throws Exception {
-            return new Tuple3<>(first.f0, first.f1, second.f1);
-          }
-        });
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(hosts * cores)
 
 
 
-//      val stream = joinedStream
-//        .where(new MyFirstKeySelector())
-//        .window(SlidingTimeWindows.of(Time.of(9, TimeUnit.SECONDS), Time.of(3, TimeUnit.SECONDS)))
-//        .equalTo(new MySecondKeySelector())
-//        .window(TumblingTimeWindows.of(Time.of(3, TimeUnit.SECONDS)))
-//        .apply(new JoinFunction());
+    //      val messageStream = env
+    //        .addSource(new FlinkKafkaConsumer082[String]("ad-events", new SimpleStringSchema(), properties))
+    //        .setParallelism(Math.min(hosts * cores, kafkaPartitions))
+    //      messageStream.rebalance
 
 
-      env.execute("flink-test")
+    //      val rawStream1 = env.socketTextStream("ginja-a1", 8800, maxRetry = -1)
+    //      val rawStream2 = env.socketTextStream("ginja-a1", 8800, maxRetry = -1)
+
+
+    // Based on https://github.com/wangyangjun/flink-stream-join/blob/master/src/test/java/fi/aalto/dmg/KafkaWindowJoinTest.java
+
+    // env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
+
+    val keySelector1 = new KeySelector[(Long, String, Int), String] {
+      override def getKey(value: (Long, String, Int)): String = value._2
     }
+
+    val age = env.fromCollection(EventTimeData.personWithAge)
+      .assignTimestamps(new TimestampExtractor[(Long, String, Int)] {
+        val MAX_DELAY = 3000
+        var currentEventTimestamp = 0l
+
+        override def getCurrentWatermark: Long = currentEventTimestamp - MAX_DELAY
+
+        override def extractWatermark(element: (Long, String, Int), currentTimestamp: Long): Long =
+          currentEventTimestamp - MAX_DELAY
+
+        override def extractTimestamp(element: (Long, String, Int), currentTimestamp: Long): Long = {
+          currentEventTimestamp = element._1
+          return currentEventTimestamp
+        }
+      })
+
+    val keySelector2 = new KeySelector[(Long, String, String), String] {
+      override def getKey(value: (Long, String, String)): String = value._2
+    }
+
+    val interest = env.fromCollection(EventTimeData.personWithInterest)
+      .assignTimestamps(new TimestampExtractor[(Long, String, String)] {
+        val MAX_DELAY = 3000
+        var currentEventTimestamp = 0l
+
+        override def getCurrentWatermark: Long = currentEventTimestamp - MAX_DELAY
+
+        override def extractWatermark(element: (Long, String, String), currentTimestamp: Long): Long =
+          currentEventTimestamp - MAX_DELAY
+
+        override def extractTimestamp(element: (Long, String, String), currentTimestamp: Long): Long = {
+          currentEventTimestamp = element._1
+          return currentEventTimestamp
+        }
+      })
+
+
+    val joinedStream = new MultiWindowsJoinedStreams[(Long, String, Int), (Long, String, String)](age.getJavaStream,
+      interest.getJavaStream)
+
+    joinedStream.where(keySelector1)
+      .window(SlidingTimeWindows.of(Time.of(25, TimeUnit.SECONDS), Time.of(5, TimeUnit.SECONDS)))
+      .equalTo(keySelector2)
+      .window(TumblingTimeWindows.of(Time.of(5, TimeUnit.SECONDS)))
+      .apply(new JoinFunction[(Long, String, Int), (Long, String, String), (String, Int, String)] {
+        override def join(first: (Long, String, Int), second: (Long, String, String)): (String, Int, String) =
+          (first._2, first._3, second._3)
+      })
+      .print()
+
+
+    env.execute("flink-test")
+  }
 
 
 }
